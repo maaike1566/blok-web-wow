6c1c1bd67e3a26edf3d384d8b82659442783ba6d
maaike1566 <maaike.bommerson@hva.nl>
2019-11-01 15:29:51 +0100

    javscript toegevoegd
    nog een nieuwere versie van de website, alleen werkt het laatste stukje javascript niet;

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,20 +1,38 @@
-//var isFoccused = document.getElementById("zoekbalk").addEventListener("focus");
-//
-//if (isFoccused) {
-//    console.log("is gefocust");
-//    document.getElementById("knop").classList.add("knopActive");
-//}
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
 
-
-
-function changeColor(){
+function changeColor() {
     document.getElementById("knop").classList.remove("knop");
     console.log("is gefocust");
     document.getElementById("knop").classList.add("knopActive");
 }
 
-function resetColor(){
-  console.log("niet gefocust");
+function resetColor() {
+    console.log("niet gefocust");
     document.getElementById("knop").classList.add("knop");
 }
 
+
+
+//wil maar niet werken
+var knop;
+var knop2 = false;
+knop = document.getElementById('download');
+
+function downloading() {
+    if (knop2 === false) {
+     document.getElementById('download').src = 'afbeeldingen/loading.gif';
+        knop2 = true;
+        setTimeout(function () {
+            this.open20();
+        }, 4000);
+        console.log("het werkt")
+    } else if (knop2 === true) {
+        document.getElementById('download').src = 'afbeeldingen/done.png';
+        console.log(done);
+        document.getElementById("download").src = done;
+        knop2 = false;
+    }
+}
+knop.addEventListener('click', downloading, false);
