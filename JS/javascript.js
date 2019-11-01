f039127ea95b9fa5b3fe78702510cbfda0a0f3d0
maaike1566 <maaike.bommerson@hva.nl>
2019-11-01 10:50:45 +0100

    kleine javascript toegevoegd
    een javascript stukje toegevoegd

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,1 +1,20 @@
+//var isFoccused = document.getElementById("zoekbalk").addEventListener("focus");
+//
+//if (isFoccused) {
+//    console.log("is gefocust");
+//    document.getElementById("knop").classList.add("knopActive");
+//}
+
+
+
+function changeColor(){
+    document.getElementById("knop").classList.remove("knop");
+    console.log("is gefocust");
+    document.getElementById("knop").classList.add("knopActive");
+}
+
+function resetColor(){
+  console.log("niet gefocust");
+    document.getElementById("knop").classList.add("knop");
+}
 
