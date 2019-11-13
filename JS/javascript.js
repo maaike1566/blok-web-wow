a62227f3466f708eaf65b3375c98bc5d42bedc65
maaike1566 <maaike.bommerson@hva.nl>
2019-11-13 22:47:11 +0100

    Javascript done
    eindelijk werkende javascript

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,37 +2,55 @@
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
 
-function changeColor() {
-    document.getElementById("knop").classList.remove("knop");
-    console.log("is gefocust");
-    document.getElementById("knop").classList.add("knopActive");
-}
-
-function resetColor() {
-    console.log("niet gefocust");
-    document.getElementById("knop").classList.add("knop");
-}
-
+//function changeColor() {
+//    document.getElementById("knop").classList.remove("knop");
+//    console.log("is gefocust");
+//    document.getElementById("knop").classList.add("knopActive");
+//}
+//
+//function resetColor() {
+//    console.log("niet gefocust");
+//    document.getElementById("knop").classList.add("knop");
+//}
 
 
 //wil maar niet werken
-var knop;
-var knop2 = false;
-knop = document.getElementById('download');
+//var knop;
+//var knop2 = false;
+//knop = document.getElementById('download');
+//
+//function downloading() {
+//    if (knop2 === false) {
+//     document.getElementById('download').src = 'afbeeldingen/loading.gif';
+//        knop2 = true;
+//        setTimeout(function () {
+//            this.open20();
+//        }, 4000);
+//        console.log("het werkt")
+//    } else if (knop2 === true) {
+//        document.getElementById('download').src = 'afbeeldingen/done.png';
+//        console.log(done);
+//        document.getElementById("download").src = done;
+//        knop2 = false;
+//    }
+//}
+//var knop = document.getElementById('download');
+// function downloading () {
+//     console.log("werkt");
+// }
+//knop.addEventListener('click', downloading, false);
 
-function downloading() {
-    if (knop2 === false) {
-     document.getElementById('download').src = 'afbeeldingen/loading.gif';
-        knop2 = true;
+window.onload = function () {
+
+    var button = document.getElementById('download');
+
+    function downloading() {
         setTimeout(function () {
-            this.open20();
+            button.src = 'afbeeldingen/done.png';
         }, 4000);
-        console.log("het werkt")
-    } else if (knop2 === true) {
-        document.getElementById('download').src = 'afbeeldingen/done.png';
-        console.log(done);
-        document.getElementById("download").src = done;
-        knop2 = false;
+        button.src = 'afbeeldingen/loading.gif';
     }
+
+    button.addEventListener('click', downloading);
+
 }
-knop.addEventListener('click', downloading, false);
